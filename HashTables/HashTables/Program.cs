cb673c81c3b9740e775b180456863d1351e68725
Mike Choi <mike.choi@live.ca>
2018-08-03 15:38:29 -0700

    Learning to use Hash Tables

---
+++ HashTables/HashTables/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables
+{
+    class Program
+    {
+        static Hashtable userInfoHash;
+
+        static void Main(string[] args)
+        {
+            userInfoHash = new Hashtable();
+
+            //Adding
+            for (int i = 0; i < 10; i++)
+            {
+                userInfoHash.Add(i, "User " + i);
+            }
+
+            //Removing
+            if (userInfoHash.ContainsKey(0))
+            {
+                userInfoHash.Remove(0);
+            }
+        }
+    }
+}
